57548b45ebb95cc7b3a495c2d5f9cdb1ebbafa47
Sourish99 <57459663+Sourish99@users.noreply.github.com>
2019-11-07 22:08:19 +0530

    Update GoBackN.java

--- GoBackN.java
+++ GoBackN.java
@@ -1,111 +1,64 @@
-package pracs;
 import java.util.*;
-import java.lang.*;
-public class GoBackN {
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the max window size");
-		int n=sc.nextInt();
-		int frame=(int)Math.pow(2,n);
-		int transmit[]=new int[frame];
-		int transmit_flag[]=new int[frame];
-		int receive[]=new int[frame-1];
-		int count1=0;
-		int count2=0;
-		int index=0;
-		
-		for(int c=0;c<n;c++)
-		{
-			transmit[c]=c;
-			transmit_flag[c]=0;
-		}
-		int inp=1;
-		do
-		{
-			System.out.println("\n1.Execute  2.EXIT");
-inp=sc.nextInt();
-switch(inp)
-{
-case 1:
-{
-count1=0;
-count2=0;
-System.out.println("\nFrames allowed");
-for(int c=index;c<n;c++)
-{
-	if(count1!=n-1)
-	{
-		System.out.println(transmit[c] +" ");
-		count1+=1;
-	}
-	
-}
-if(count1!=n-1)
-{
-	for(int d=0, l=1;l<=n-count1;d++,l++)
-	{
-		System.out.println(transmit[d] + " ");
-		count1+=1;
-	}
-}
 
+class go{
 
-	
-System.out.println("/nThe tansmitted frames");
-for(int c=0;c<n-1;c++)
-{
-	receive[c]=sc.nextInt();
-}
-System.out.println("\nMax window size reached "+index);
-for(int c=0;c<n-1;c++)
-{
-	if(receive[c]==transmit[index])
-	{
-		transmit_flag[index]=1;
-		System.out.println(receive[c]+" IS ACCEPTED");
-		if(index==n-1)
-		{
-			index=0;
-		}
-		else
-		{
-			index+=1;
-		}
-	}
-	else
-	{
-		System.out.println(receive[c]+" IS NOT ACCEPTED");
-		break;
-	}
-}
+    public static void main(String[] args)
+    {
+        System.out.print("Enter No. of Bits in the Sequence Number: ");
+        Scanner sc= new Scanner(System.in);
+        int k1 = sc.nextInt();
+        
+        int size =((int)Math.pow(2,k1))-1;
+        int a[] = new int[size];
 
-for(int c=index;c<n;c++)
-{
-	transmit_flag[c]=0;
-	count2+=1;
-}
-if(count2!=n)
-{
-	for(int d=0,l=1;l<=n-count2;d++,l++)
-	{
-		transmit_flag[d]=0;
-	}
-}
-break;
+        System.out.println("Max Window Size: "+size);
+        int c=0;
+        int k=0;
 
+        while(k<size)
+        {
+        System.out.println("-----------");
+        
+        System.out.println("Sender");
+        System.out.println("-----------");
+        System.out.println("Frames Transmitted :");
+        for(int i=c;i<size;i++)
+        {
+            System.out.print(i+"\t");
+        }
+        System.out.println("\n-----------");
+        System.out.println("Receiver");
+        System.out.println("-----------");
+        System.out.println("Frames Received (1-Yes, 0-No)");
+        for(int i=c;i<size;i++)
+        {
+            System.out.println("Frame "+i);
+            a[i]=sc.nextInt();
+        }
+        System.out.println("-----------");
+        System.out.println("Sender");
+        System.out.println("-----------");
+        
+        for(k=c;k<size;k++)
+        {
+            if(a[k]==1)
+            {
+                System.out.println("Frame "+k+" is Acknowledged");
+                
+            }
+            else
+            {
+                System.out.println("Frame "+k+" is Not Acknowledged/ Lost");
+                c=k;
+                
+                break;
+            
+
+            }
+
+        }
+
+    }
+
+    }
 }
-case 2:
-{
-	System.out.println("\nEXITED");
-	break;
-}
-default:
-{
-	System.out.println("\nInvalid input");
-	break;
-}
-}		
-		}while(inp!=2);
-		}
-	}
